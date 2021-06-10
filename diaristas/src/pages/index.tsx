51b0e33c4a64729ee6563bc8565c08bff042547d
AllisonVinicius <allisonvinicius63@gmail.com>
2021-06-10 00:42:15 -0400

    adicionando component

--- diaristas/src/pages/index.tsx
+++ diaristas/src/pages/index.tsx
@@ -1,11 +1,17 @@
 
 import SafeEnvironment from 'ui/components/feedBack/SafeEnvironment/SafeEnvironment';
-
+import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 export default function Home() {
   return (
     <div>
+      
       <SafeEnvironment />
-
+      <PageTitle 
+        title={'Conheça os Profissionais'}
+        subtitle={' endereço Profissionais Localidade'}
+        
+      />
+     
     </div>
-  )
+  );
 }
