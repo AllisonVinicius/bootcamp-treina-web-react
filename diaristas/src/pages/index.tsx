741cf6af7247ffa8cf7bade4abdcef27c56cc117
AllisonVinicius <allisonvinicius63@gmail.com>
2021-06-10 21:52:15 -0400

    chamando o component

--- diaristas/src/pages/index.tsx
+++ diaristas/src/pages/index.tsx
@@ -1,17 +1,26 @@
-
-import SafeEnvironment from 'ui/components/feedBack/SafeEnvironment/SafeEnvironment';
-import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
+import SafeEnvironment from "ui/components/feedBack/SafeEnvironment/SafeEnvironment";
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
+import UserInformation from "ui/components/data-display/UserInformation/UserInformations";
 export default function Home() {
   return (
     <div>
-      
       <SafeEnvironment />
-      <PageTitle 
-        title={'Conheça os Profissionais'}
-        subtitle={' endereço Profissionais Localidade'}
-        
+      <PageTitle
+        title={"Conheça os Profissionais"}
+        subtitle={" endereço Profissionais Localidade"}
       />
-     
+      <UserInformation
+        name={"Allison Vinicius"}
+        picture={"https://github.com/AllisonVinicius.png"}
+        rating={3}
+        description={"Corumba - Ms"}
+      />
+      <UserInformation
+        name={" Vinicius"}
+        picture={"https://github.com/AllisonVinicius.png"}
+        rating={2}
+        description={"Corumba - Ms"}
+      />
     </div>
   );
 }
