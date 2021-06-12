c4eaf95c5bad3b5714988917a63a73d618deb3c4
AllisonVinicius <allisonvinicius63@gmail.com>
2021-06-11 23:43:37 -0400

    estilizando e invocando o component

--- diaristas/src/ui/components/surface/Footer/Footer.tsx
+++ diaristas/src/ui/components/surface/Footer/Footer.tsx
@@ -1,21 +1,38 @@
 import React from "react";
-import { FooterStyled } from "./styles";
+import { AppList, FooterStyled } from "./styles";
 // import { Container } from './styles';
 import { FooterContainer, FooterTitle } from "./styles";
-import { Typography } from "@material-ui/core";
+import { Typography, Box } from "@material-ui/core";
 
 const Footer: React.FC = () => {
   return (
     <FooterStyled>
       <FooterContainer>
-        <FooterTitle>Quem Somos</FooterTitle>
-        <Typography>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed iusto
-          repudiandae ipsam, illo reprehenderit error eligendi dignissimos
-          aspernatur earum obcaecati molestias nulla similique aperiam? Delectus
-          voluptatem officiis sequi maxime soluta?
-        </Typography>
-        <FooterTitle>Baixe nossos App</FooterTitle>
+        <Box sx={{ maxWidth: "400px" }}>
+          <FooterTitle>Quem Somos</FooterTitle>
+          <Typography variant={"body2"} sx={{ mt: 2 }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed iusto
+            repudiandae ipsam, illo reprehenderit error eligendi dignissimos
+            aspernatur earum obcaecati molestias nulla similique aperiam?
+            Delectus voluptatem officiis sequi maxime soluta?
+          </Typography>
+        </Box>
+
+        <div>
+          <FooterTitle>Baixe nossos App</FooterTitle>
+          <AppList>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"noopener noreferrer"}>
+                <img src={"/img/logos/app-store.png"} alt={"app "} />
+              </a>
+            </li>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"noopener noreferrer"}>
+                <img src={"/img/logos/google-play.png"} alt={"app "} />
+              </a>
+            </li>
+          </AppList>
+        </div>
       </FooterContainer>
     </FooterStyled>
   );
