ab5e7790c3741840389ddc9368970af247a7350f
AllisonVinicius <allisonvinicius63@gmail.com>
2021-06-13 23:19:06 -0400

    alteracoes

--- diaristas/src/ui/components/input/TextFild/TextFild.tsx
+++ diaristas/src/ui/components/input/TextFild/TextFild.tsx
@@ -3,8 +3,4 @@ import React from "react";
 // import { Container } from './styles';
 import { TextFieldStyles } from "./styles";
 
-const TextFild: React.FC = () => {
-  return <TextFieldStyles></TextFieldStyles>;
-};
-
-export default TextFild;
+export default TextFieldStyles;
